be2d9f82b8c19c70965bbd4fa3bb31ec1284f737
pasquale <pasqualepolverino98@gmail.com>
2023-06-11 11:55:38 +0000

    fix

--- run-experiments.sh
+++ run-experiments.sh
@@ -5,8 +5,8 @@ echo "Compiling stuff..."
 make
 
 for j in $(seq 1 50); do
-    for i in $(seq 2 10 1000); do
-        ./main --nodes="$i" --mode="$1"
+    for i in $(seq 2 10 500); do
+        ./build/synthetic-tests-simulator --nodes="$i" --graph-generator="$1" --aglorithm="$2"
     done
 done
 
